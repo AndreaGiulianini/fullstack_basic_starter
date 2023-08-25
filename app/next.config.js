6d956c1bfa7f9380439f851f2ecc6f143a1d622a
Andrea Giulianini <andreagiulianini97@gmail.com>
2023-08-25 16:20:20 +0200

    fix

--- app/next.config.js
+++ app/next.config.js
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
   env: {
     NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
   },
