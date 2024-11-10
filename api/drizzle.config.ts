7571b1158a22c576826248c302ef764a7bb09832
Andrea Giulianini <andrea.giulianini@cloov.tech>
2024-11-10 02:47:56 +0100

    update config

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: './src/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_POST}/${process.env.DB_USNAME}`
+    url: `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
   }
 })
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 set -eu
 
 cp compose_override/development.yml docker-compose.override.yml
-docker compose down && docker compose up --build
+docker compose down && docker compose watch
