deef94dc6e86e1a5354c359a8da248d92863cde5
Andrea Giulianini <andrea.giulianini@cloov.tech>
2024-11-12 10:24:58 +0100

    update next15

--- app/src/components/counter/Counter.tsx
+++ app/src/components/counter/Counter.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,14 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       borderRadius: {
